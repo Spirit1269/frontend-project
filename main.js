4cab8483c32b1f4aa9e6096df2696ae6dae34325
Spirit1269 <kimberlydjenkins1@gmail.com>
2023-03-17 10:50:14 -0500

    five

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ let $name = $("#symbol");
 let $value = $('#dropDown');
 let $button = $("#submit")
 let $results = $("#results")
+let today =  new Date().toISOString().slice(0, 10)
 
-
-// console.log(name)
+console.log(today)
  
 $button.on("click", function(event){
     getValues(event);
@@ -16,46 +16,30 @@ $value.on("change", function(event){
 
 function getValues(event) {
     event.preventDefault();
-//     let name = $name.val().toUpperCase();
-//    selectedValue = $value.val();
- 
-//   if (selectedValue) {
-//     // If a value is selected from dropdown, use it instead of input value
-//     name = selectedValue;
-//   }
-  
-//   if (!name) {
-//     alert("Please enter a symbol or select one from the list.");
-//     return;
-//   }
-let name = $name.val().toUpperCase();
-let selectedValue = $value.val();
-    
-if (!name && selectedValue === 'choose') {
-    alert("Please enter a symbol or select one from the list.");
-    return;
-}
 
-if (name && selectedValue !== 'Please choose') {
-    // If both input field and dropdown have values, use dropdown value
-    name = selectedValue;
-}
+    let name = $name.val().toUpperCase();
+    let selectedValue = $value.val();
+    
+    if (!name && selectedValue === 'Please choose') {
+        alert("Please enter a symbol or select one from the list.");
+        return;
+    }
+
+    if (name && selectedValue !== 'Please choose') {
+        // If both input field and dropdown have values, use dropdown value
+        name = selectedValue;
+    }
+
     console.log(name)
     console.log(selectedValue)
-    // if (selectedValue && name) {
-    //     // Both drop-down menu and input field have values, use drop-down menu value
-    //     name = selectedValue;
-    // } else if (!selectedValue && !name) {
-    //     alert("Please enter a symbol or select one from the list.");
-    //     return;
-    // }
+    
      $.get(`https://financialmodelingprep.com/api/v3/quote/${name}?apikey=8672d4bf71053ec270bb774352caec83` , (data) => {
-    $results.empty();
+        $results.empty();
     console.log(data);
     let results = data[0];
     if (data.length === 0){
        alert("The symbol cannot be found, please search a different symbol!");
-    //    return;
+  
     }else {
         let $card = $('<div>').addClass('result-card');
         let $cardTitle = $('<h4>').text(`NAME:  ${results.name}`);
@@ -66,7 +50,7 @@ if (name && selectedValue !== 'Please choose') {
         let $cardOpen = $('<h4>').text(`OPEN PRICE:  ${results.open}`);
         let $cardEarnAnn = $('<h4>').text(`EARNINGS ANNOUNCEMENT:  ${results.earningsAnnouncement}`)
         let $cardExchange = $('<h4>').text(`EXCHANGE:  ${results.exchange}`);
-        $card.append($cardTitle, $cardPrice, $cardOpen, $cardChange, $cardDayLow, $cardDayHigh, $cardEarnAnn, $cardExchange)
+        $card.append( today, $cardTitle, $cardPrice, $cardOpen, $cardChange, $cardDayLow, $cardDayHigh, $cardEarnAnn, $cardExchange)
         $results.append($card)
     }
     })
